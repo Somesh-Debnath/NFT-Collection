0cf6c8d6230661a091347323614ba34e07d887eb
Somesh Debnath <91829500+Somesh-Debnath@users.noreply.github.com>
2022-05-29 23:58:06 +0530

    Created Smart Contracts and Deployed in Rinkeby Testnet

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x51620532C6941d93EE75B388d4735360Ed670B3a";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
